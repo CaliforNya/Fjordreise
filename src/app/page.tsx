fc09ffe19aa6b9f6ff7058714ac1087f14b5fceb
Monika <maam092024@gmail.com>
2026-04-08 12:59:20 +0200

    global.css: heading and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import SearchForm from "@/components/SearchForm";
 export default function HomePage() {
   return (
     <main>
-      <h1>Search</h1>
-      <SearchForm />
+      <div className="content-wrapper page-stack">
+        <h1>Search</h1>
+        <SearchForm />
+      </div>
     </main>
   );
 }
